26bc9262b25a117f772e45c35f377a64f1f54db5
mehdi <mehdi.dridi@g-portal.com>
2025-12-16 10:07:59 +0100

    push to stack

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -20,4 +20,35 @@ public class Stack {
 		this.top = node;
 		this.height = 1;
 	}
+
+	public void push(int value) {
+		Node stackNode = new Node(value);
+
+		if (height == 0) {
+			top = stackNode;
+			height++;
+		} else {
+			stackNode.next = top;
+			top = stackNode;
+			height++;
+		}
+
+	}
+
+	public void printStack() {
+		Node tmp = top;
+		for (int i = 0; i <= height - 1; i++) {
+			System.out.println(tmp.value);
+			tmp = tmp.next;
+		}
+	}
+
+	public void getTop() {
+		System.out.println("Top: " + this.top.value);
+	}
+
+	public void getHeight() {
+		System.out.println("Height: " + this.height);
+	}
+
 }
