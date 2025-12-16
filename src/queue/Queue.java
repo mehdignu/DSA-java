dd10e84151434db1f2ddf0b9f7a3f127d18dd435
mehdi <mehdi.dridi@g-portal.com>
2025-12-16 11:33:57 +0100

    enqueue and dequeue

--- src/queue/Queue.java
+++ src/queue/Queue.java
@@ -31,6 +31,36 @@ public class Queue {
 		}
 	}
 
+	public void enqueue(int value) {
+		Node newNode = new Node(value);
+
+		if (length == 0) {
+			first = newNode;
+			last = newNode;
+		} else {
+			last.next = newNode;
+			last = newNode;
+			length++;
+		}
+		length++;
+	}
+
+	public Node dequeue() {
+		if (length == 0)
+			return null;
+		Node tmp = first;
+		if (length == 1) {
+			first = null;
+			last = null;
+		} else {
+			first = first.next;
+			tmp.next = null;
+		}
+
+		length--;
+		return tmp;
+	}
+
 	public void printFirst() {
 		System.out.println("First: " + this.first.value);
 	}
