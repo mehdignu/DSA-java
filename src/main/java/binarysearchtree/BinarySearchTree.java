e98c2eca65af1abe4f9b84d6d06e0f3d1fb8b2d7
mehdi <dridignu@gmail.com>
2026-01-06 12:26:28 +0100

    add BFS to the binary tree

--- src/main/java/binarysearchtree/BinarySearchTree.java
+++ src/main/java/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,9 @@
 package binarysearchtree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinarySearchTree {
 
 	class Node {
@@ -13,6 +17,24 @@ public class BinarySearchTree {
 	}
 
 	Node root;
+
+	public ArrayList<Integer> BFS() {
+		Node currentNode = root;
+		Queue<Node> queue = new LinkedList<>();
+		ArrayList<Integer> results = new ArrayList<>();
+		queue.add(currentNode);
+		while (queue.size() > 0) {
+			currentNode = queue.remove();
+			results.add(currentNode.value);
+			if (currentNode.left != null) {
+				queue.add(currentNode.left);
+			}
+			if (currentNode.right != null) {
+				queue.add(currentNode.right);
+			}
+		}
+		return results;
+	}
 
 	private Node rInsert(Node currentNode, int value) {
 		if (currentNode == null)
